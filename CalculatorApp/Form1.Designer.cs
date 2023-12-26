640f24b50ab32fe644b0d3b933197d4eede6a4f9
marcusz <marcusz@INSPIRON>
2023-12-26 20:39:08 +0800

    Add project files.

---
+++ CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            button16 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Space Grotesk", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(21, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(285, 34);
+            textBox1.TabIndex = 0;
+            textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(26, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 56);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(121, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 56);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(219, 90);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 56);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(26, 171);
+            button4.Name = "button4";
+            button4.Size = new Size(72, 56);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(121, 171);
+            button5.Name = "button5";
+            button5.Size = new Size(72, 56);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click_1;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(219, 171);
+            button6.Name = "button6";
+            button6.Size = new Size(72, 56);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click_1;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(26, 247);
+            button7.Name = "button7";
+            button7.Size = new Size(72, 56);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click_1;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(121, 247);
+            button8.Name = "button8";
+            button8.Size = new Size(72, 56);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click_1;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(219, 247);
+            button9.Name = "button9";
+            button9.Size = new Size(72, 56);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click_1;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.Location = new Point(84, 407);
+            button10.Name = "button10";
+            button10.Size = new Size(72, 56);
+            button10.TabIndex = 10;
+            button10.Text = "+";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.Location = new Point(84, 469);
+            button11.Name = "button11";
+            button11.Size = new Size(72, 56);
+            button11.TabIndex = 11;
+            button11.Text = "-";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button12.Location = new Point(26, 318);
+            button12.Name = "button12";
+            button12.Size = new Size(72, 56);
+            button12.TabIndex = 12;
+            button12.Text = "=";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button13.Location = new Point(162, 469);
+            button13.Name = "button13";
+            button13.Size = new Size(72, 56);
+            button13.TabIndex = 13;
+            button13.Text = "/";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button14.Location = new Point(162, 407);
+            button14.Name = "button14";
+            button14.Size = new Size(72, 56);
+            button14.TabIndex = 14;
+            button14.Text = "X";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button15.Location = new Point(121, 318);
+            button15.Name = "button15";
+            button15.Size = new Size(72, 56);
+            button15.TabIndex = 15;
+            button15.Text = "0";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Variable Display Semil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(328, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 43);
+            label1.TabIndex = 16;
+            label1.Text = "More Modes";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(339, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(354, 15);
+            label2.TabIndex = 17;
+            label2.Text = "In the meantime, you can just do a basic calculation tasks for now";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Variable Display Semil", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(509, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(204, 43);
+            label3.TabIndex = 18;
+            label3.Text = "Coming Soon";
+            label3.Click += label3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(373, 158);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(340, 320);
+            pictureBox1.TabIndex = 22;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Space Grotesk", 21.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(339, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 41);
+            label4.TabIndex = 20;
+            label4.Click += label4_Click;
+            // 
+            // button16
+            // 
+            button16.Font = new Font("Space Grotesk", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button16.Location = new Point(219, 318);
+            button16.Name = "button16";
+            button16.Size = new Size(72, 56);
+            button16.TabIndex = 21;
+            button16.Text = "AC";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(770, 548);
+            Controls.Add(button16);
+            Controls.Add(label4);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            RightToLeft = RightToLeft.No;
+            Text = "Calculator App";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Button button16;
+    }
+}
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System.Data;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Number buttons
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        // Plus button
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+        }
+
+        // Multiply button
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "*";
+        }
+
+        // Divide button
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "/";
+        }
+
+        // Minus button
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "-";
+        }
+
+        // Equals button
+        public void button12_Click(object sender, EventArgs e)
+        {
+            // Evaluate the expression and display the result
+            try
+            {
+                new DataTable().Compute(textBox1.Text, null);
+            }
+            catch (Exception SyntaxErrorException)
+            {
+                textBox1.Text = "Invalid expression";
+                return;
+            }
+
+            // Recalculate the result
+            var result = new DataTable().Compute(textBox1.Text, null);
+
+            // Print the results
+            if (result.ToString() == "∞")
+            {
+                textBox1.Text = "Cannot divide by zero";
+            }
+            else
+            {
+                textBox1.Text = result.ToString();
+            }
+        }
+
+        // Easter Egg UWU
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Gerald Kase";
+        }
+
+        // LMAO
+        private void label4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.GeraldoBTD69;
+        }
+
+        // AC Button
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
